b5b14e2ea6c5fc2d24a7a667ec34d931031de979
Tomatobird8 <60841151+Tomatobird8@users.noreply.github.com>
2026-02-24 23:43:09 +0200

    j

--- MineshaftRescaler.cs
+++ MineshaftRescaler.cs
@@ -1,6 +1,7 @@
 using BepInEx;
 using BepInEx.Logging;
 using HarmonyLib;
+using MineshaftRescaler.Patches;
 
 namespace MineshaftRescaler
 {
@@ -11,22 +12,31 @@ namespace MineshaftRescaler
         internal new static ManualLogSource Logger { get; private set; } = null!;
         internal static Harmony? Harmony { get; set; }
 
+        public static bool applyMineshaftSizeChanges;
         public static int minLength;
         public static int maxLength;
         public static float mainLength;
         public static float cavesLength;
         public static float fireLength;
 
+        public static bool changeMineshaftLootBonus;
+        public static int lootAmountBonus;
+
         private void Awake()
         {
             Logger = base.Logger;
             Instance = this;
 
+            applyMineshaftSizeChanges = Config.Bind<bool>("General", "ApplyMineshaftSizeChanges", true, "Applies the mineshaft size changes listed below.").Value;
             minLength = Config.Bind<int>("General", "MinLength", 14, "Minimum generation length").Value;
             maxLength = Config.Bind<int>("General", "MaxLength", 18, "Maximum generation length").Value;
             mainLength = Config.Bind<float>("General", "MainLength", 0.25f, "Relative length of the Main side section.").Value;
             cavesLength = Config.Bind<float>("General", "CavesLength", 0.6f, "Relative length of the caves section.").Value;
             fireLength = Config.Bind<float>("General", "FireLength", 0.15f, "Relative length of the Fire side section.").Value;
+
+            changeMineshaftLootBonus = Config.Bind<bool>("General", "ChangeMineshaftLootBonus", false, "Change the bonus amount of loot to spawn in the mineshaft interior.").Value;
+            lootAmountBonus = Config.Bind<int>("General", "LootAmountBonus", 6, "Amount of extra loot to spawn in mineshaft.").Value;
+
 
             Patch();
 
@@ -37,9 +47,16 @@ namespace MineshaftRescaler
         {
             Harmony ??= new Harmony(MyPluginInfo.PLUGIN_GUID);
 
-            Logger.LogDebug("Patching...");
-
-            Harmony.PatchAll();
+            if (applyMineshaftSizeChanges)
+            {
+                Logger.LogDebug($"Patching... {nameof(PreGenerateDungeonPatch)}");
+                Harmony.PatchAll(typeof(PreGenerateDungeonPatch));
+            }
+            if (changeMineshaftLootBonus)
+            {
+                Logger.LogDebug($"Patching... {nameof(RoundManager)}");
+                Harmony.PatchAll(typeof(RoundManagerPatch));
+            }
 
             Logger.LogDebug("Finished patching!");
         }
---
+++ Patches/RoundManagerPatch.cs
@@ -0,0 +1,38 @@
+﻿using HarmonyLib;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+
+namespace MineshaftRescaler.Patches
+{
+    [HarmonyPatch(typeof(RoundManager))]
+    internal class RoundManagerPatch
+    {
+        [HarmonyPatch("SpawnScrapInLevel")]
+        [HarmonyTranspiler]
+        internal static IEnumerable<CodeInstruction> ChangeMinshaftLootBonus(IEnumerable<CodeInstruction> instructions)
+        {
+            int index = -1;
+            var codes = new List<CodeInstruction>(instructions);
+
+            for (int i = 10; i < codes.Count; i++)
+            {
+                if (codes[i - 2].opcode == OpCodes.Ldloc_1 && codes[i - 1].opcode == OpCodes.Ldc_I4_6 && codes[i].opcode == OpCodes.Add)
+                {
+                    index = i - 1;
+                    break;
+                }
+            }
+            if (index > -1)
+            {
+                codes[index].opcode = OpCodes.Ldc_I4_S;
+                codes[index].operand = (sbyte)MineshaftRescaler.lootAmountBonus;
+                MineshaftRescaler.Logger.LogDebug("Successfully injected new loot bonus count.");
+            }
+            else
+            {
+                MineshaftRescaler.Logger.LogError("Failed to inject new mineshaft loot bonus amount. ldcloc.1 -> ldc.i4.6 -> add not found in RoundManager.SpawnScrapInLevel.");
+            }
+            return codes;
+        }
+    }
+}
